c753fa8e1e99d8f207f7cef05386cafff464a92c
dylanCapeTown <43037431+dylanCapeTown@users.noreply.github.com>
2020-02-01 21:57:39 +0200

    20200201

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Listing
+
+class ListingAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'is_published', 'price', 'list_date', 'realtor')
+    list_display_links = ('id', 'title')
+
+admin.site.register(Listing, ListingAdmin)
